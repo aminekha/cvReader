9a9316b5094ccf9d243b7376efec32da874c0b19
Amine Khaoui <aminekhaoui97@gmail.com>
2023-07-05 09:50:11 +0100

    Read all files in a folder

--- reader/views.py
+++ reader/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    if request.method == 'POST':
+        files = request.FILES.getlist('files')
+        file_names = [file.name for file in files]
+        return render(request, 'reader/index.html', {'file_names': file_names})
+    
+    return render(request, 'reader/index.html')
