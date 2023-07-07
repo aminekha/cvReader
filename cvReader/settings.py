b302ba3a1777d577cd99c9353f3eb29e44633f77
Amine Khaoui <aminekhaoui97@gmail.com>
2023-07-07 17:59:58 +0100

    Add static url

--- cvReader/settings.py
+++ cvReader/settings.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -105,7 +105,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+PROJECT_ROOT = os.path.normpath(os.path.dirname(__file__))
+STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
