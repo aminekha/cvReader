c1ded32ec2275ab8bb88a706b9475a9683f02549
Amine Khaoui <aminekhaoui97@gmail.com>
2023-07-10 11:15:44 +0100

    Added Windows config

---
+++ install.bat
@@ -0,0 +1,13 @@
+@echo off
+
+REM Step 1: Install Python dependencies
+pip install -r requirements.txt
+
+REM Step 2: Run Django migrations
+python manage.py migrate
+
+REM Step 3: Run Django local server
+start "" python manage.py runserver
+
+REM Step 4: Open Chrome with the local server URL
+start chrome http://localhost:8000
