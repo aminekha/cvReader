fcf01ad24ff18c652e23c010cbc47637146a49a8
Amine Khaoui <aminekhaoui97@gmail.com>
2023-07-13 14:45:08 +0100

    Add bonus fileds

--- reader/views.py
+++ reader/views.py
@@ -8,32 +8,11 @@ import docx2txt
 from django.shortcuts import render
 
 
-countries = ["France", "Londres"]
-france_cities = [
-    "Paris", "Marseille", "Lyon", "Toulouse", "Nice", "Nantes", "Strasbourg", "Montpellier", "Bordeaux", "Lille",
-    "Rennes", "Reims", "Saint-Étienne", "Toulon", "Le Havre", "Clermont-Ferrand", "Limoges", "Tours", "Amiens",
-    "Metz", "Perpignan", "Besançon", "Orléans", "Mulhouse", "Caen", "Nancy", "Rouen", "Argenteuil", "Saint-Denis",
-    "Roubaix", "Tourcoing", "Montreuil", "Avignon", "Asnières-sur-Seine", "Nanterre", "Poitiers", "Versailles",
-    "Courbevoie", "Créteil", "Pau", "Colombes", "Vitry-sur-Seine", "Aulnay-sous-Bois", "Marseille 10", "Marseille 11",
-    "Marseille 12", "Marseille 13", "Marseille 14", "Marseille 15", "Marseille 16"
-]
-
-# List of the biggest 50 cities in London (approximate population)
-london_cities = [
-    "London", "Croydon", "Bromley", "Barnet", "Enfield", "Harrow", "Wandsworth", "Greenwich", "Lewisham", "Hackney",
-    "Haringey", "Newham", "Redbridge", "Havering", "Hillingdon", "Hounslow", "Kingston upon Thames", "Merton",
-    "Sutton", "Bexley", "Brent", "Ealing", "Hammersmith and Fulham", "Harrow", "Hillingdon", "Hounslow", "Kensington and Chelsea",
-    "Kingston upon Thames", "Lambeth", "Lewisham", "Merton", "Newham", "Richmond upon Thames", "Southwark", "Sutton",
-    "Tower Hamlets", "Waltham Forest", "Wandsworth", "Westminster"
-]
-
-# Combine the city lists
-cities = france_cities + london_cities
 # Load spaCy's English model
 # nlp = spacy.load('en_core_web_sm')
 nlp = spacy.load('fr_core_news_sm')
 
-def extract_data(file_path, file_name, keywords_list, coefficients, is_country):
+def extract_data(file_path, file_name, keywords_list, coefficients):
     if file_name.lower().endswith('.pdf'):
         # Extract data from PDF
         doc = fitz.open(file_path)
@@ -52,25 +31,6 @@ def extract_data(file_path, file_name, keywords_list, coefficients, is_country):
     else:
         # Unsupported file format
         return None
-
-    # Process the content using spaCy
-    country = None
-    city = []
-    age = None
-
-    # Extract relevant information (country, city, age)
-
-    if is_country:
-        doc = nlp(content)
-        for ent in doc.ents:
-            if ent.label_ == 'LOC' and len(ent.text) > 4:
-                if not country and ent.text in countries:
-                    country = ent.text
-                elif ent.text not in city and ent.text in cities:
-                    # city = ent.text
-                    city.append(ent.text.lower())
-            elif ent.label_ == 'CARDINAL' and not age:
-                age = ent.text
     
     # Extract keywords
     keyword_counts = {keyword.lower(): 0 for keyword in keywords_list}
@@ -79,18 +39,20 @@ def extract_data(file_path, file_name, keywords_list, coefficients, is_country):
         keyword_counts[keyword.lower()] = content.count(keyword.lower()) * int(coefficients[i])
 
 
-    return country, city, age, keyword_counts
+    return keyword_counts
 
 
 def index(request):
     if request.method == 'POST':
         files = request.FILES.getlist('files')
-        country = request.POST.get('country', '')
-        city = request.POST.get('city', '')
-        age = request.POST.get('age', '')
+        # country = request.POST.get('country', '')
+        # city = request.POST.get('city', '')
+        # age = request.POST.get('age', '')
         # keywords = [keyword.strip() for keyword in request.POST.get('keywords', '').split(',') if keyword.strip()]
+        path = request.POST.get('path', '')
         keywords = request.POST.getlist('keywords[]')
         coefficients = request.POST.getlist('coefficients[]')
+        bonus = request.POST.getlist('bonus[]')
 
         filtered_files = []
 
@@ -103,7 +65,7 @@ def index(request):
                     temp_file_path = temp_file.name
                     print(temp_file_path)
 
-                file_country, file_city, file_age, file_keywords = extract_data(temp_file_path, file.name, keywords, coefficients, country)
+                file_keywords = extract_data(temp_file_path, file.name, keywords, coefficients)
                 # print(file_country, file_city, file_age)
                 # print(file_keywords)
 
@@ -111,35 +73,18 @@ def index(request):
                 total_count = sum(file_keywords.values())
 
                 # Give bonus if it has more than one keyword
-                bonus = 0
                 num_keywords_with_counts = sum(1 for count in file_keywords.values() if count > 0)  # Count the number of keywords with counts greater than 0
-                if num_keywords_with_counts == 2:
-                    bonus = 5
+                b = 0
+                if num_keywords_with_counts == 1:
+                    b = 0
+                elif num_keywords_with_counts == 2:
+                    b = 1
                 elif num_keywords_with_counts == len(keywords):
-                    bonus = 10
+                    b = 2
 
-                total_count += bonus
+                total_count += int(bonus[b])
 
-
-                if(file_country):
-                    if(file_country.lower() == country.lower()):
-                        print(file.name)
-
-                # Filtering based on extracted information
-                if(file_country):
-                    if country and country.lower() != file_country.lower():
-                        os.remove(temp_file_path)
-                        continue
-                # else:
-                #         os.remove(temp_file_path)
-                #         continue
-                if(len(file_city) > 0):
-                    if city and city.lower() not in file_city:
-                        os.remove(temp_file_path)
-                        continue
-                # if age and age != file_age:
-                #     os.remove(temp_file_path)
-                #     continue
+                path = path + "/" + file.name
 
                 filtered_files.append(
                     {
@@ -147,8 +92,8 @@ def index(request):
                         "keyword1_total": file_keywords[keywords[0]],
                         "keyword2_total": file_keywords[keywords[1]],
                         "keyword3_total": file_keywords[keywords[2]],
-                        "bonus": bonus,
-                        "path": temp_file_path,
+                        "bonus": bonus[b],
+                        "path": path,
                         "total": total_count
                     }
                 )
